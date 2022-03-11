27da7ccf8c1e589d4d7d38ed1ff43353e1dfaf5c
PaulStanos <p.stanos@techsmith.com>
2022-03-11 15:28:50 -0500

    + Query to retrieve information on the state of stats across the database

--- proc_RunUpdateStatistics.sql
+++ proc_RunUpdateStatistics.sql
@@ -34,6 +34,33 @@ AS
 DECLARE @TableName NVARCHAR(257);
 DECLARE @sql NVARCHAR(MAX);
 
+SET @v_GetStatisticsCmd = '
+   SELECT
+      s.name AS SchemaName
+      ,o.name AS TableName
+      ,i.name AS IndexName
+      ,stats_props.last_updated AS StatsLastUpdatedTime
+      ,stats_props.unfiltered_rows AS RowCountOnLastStatsUpdate
+   FROM
+      ['+ @p_DatabaseName +'].sys.stats AS stats
+   INNER JOIN
+      ['+ @p_DatabaseName +'].sys.objects AS o ON o.object_id = stats.object_id
+   INNER JOIN
+      ['+ @p_DatabaseName +'].sys.schemas AS s ON s.schema_id = o.schema_id
+   INNER JOIN
+      ['+ @p_DatabaseName +'].sys.indexes AS i ON i.object_id = o.object_id AND stats.stats_id = i.index_id -- Stats ID corresponds to index ID when stats are for an index
+   INNER JOIN
+      ['+ @p_DatabaseName +'].INFORMATION_SCHEMA.TABLES AS t ON t.TABLE_SCHEMA = s.name AND t.TABLE_NAME = o.name AND t.TABLE_TYPE = ''BASE TABLE'' -- Remove system tables from list
+   CROSS APPLY
+      ['+ @p_DatabaseName +'].sys.dm_db_stats_properties(o.object_id, stats.stats_id) AS stats_props
+   WHERE
+      i.auto_created = 0
+   AND
+      stats.auto_created = 0
+   ORDER BY
+      stats_props.last_updated
+   ASC';
+
 DECLARE TableCursor CURSOR READ_ONLY FOR
 SELECT
     TABLE_SCHEMA + '.' + TABLE_NAME
